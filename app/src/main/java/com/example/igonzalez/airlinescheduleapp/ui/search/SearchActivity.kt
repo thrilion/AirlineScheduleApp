4e3ccaf5504076c85c10bd33bea452e928868781
ismagonzalezlopez <ismagonzalezlopez@gmail.com>
2019-01-30 21:54:04 +0100

    UPDATE Search Activity, adding origin, destination and date selection

--- app/src/main/java/com/example/igonzalez/airlinescheduleapp/ui/search/SearchActivity.kt
+++ app/src/main/java/com/example/igonzalez/airlinescheduleapp/ui/search/SearchActivity.kt
@@ -2,12 +2,18 @@ package com.example.igonzalez.airlinescheduleapp.ui.search
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.example.igonzalez.airlinescheduleapp.R
 import com.example.igonzalez.airlinescheduleapp.ui.base.BaseActivity
 import com.example.igonzalez.airlinescheduleapp.ui.schedule.ScheduleActivity
 import kotlinx.android.synthetic.main.activity_search.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class SearchActivity : BaseActivity<SearchPresenter>(), SearchView {
+
+    private lateinit var datePicked: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +21,7 @@ class SearchActivity : BaseActivity<SearchPresenter>(), SearchView {
 
         presenter.makeAuthTokenRequest()
 
-        btn_search.setOnClickListener {
-            beginSearch()
-        }
+        configView()
     }
 
     override fun setPresenter(): SearchPresenter {
@@ -26,15 +30,43 @@ class SearchActivity : BaseActivity<SearchPresenter>(), SearchView {
 
     override fun saveAuthToken(accessToken: String) {
         val sharedPref = this.getSharedPreferences(
-            getString(R.string.preference_file_key), Context.MODE_PRIVATE) ?: return
-        with (sharedPref.edit()) {
+            getString(R.string.preference_file_key), Context.MODE_PRIVATE
+        ) ?: return
+        with(sharedPref.edit()) {
             putString("API_ACCESS_TOKEN", "Bearer $accessToken")
             apply()
         }
     }
 
+    private fun configView() {
+        // Calendar
+        val calendar = Calendar.getInstance()
+        calendar_view_search_calendar.minDate = calendar.time.time
+        val format = SimpleDateFormat("yyyy-MM-dd")
+        datePicked = format.format(calendar.time)
+        calendar_view_search_calendar.setOnDateChangeListener { _, year, month, day ->
+            dateFormat(year, month, day)
+        }
+
+        // Search button
+        btn_search_search.setOnClickListener {
+            beginSearch()
+        }
+    }
+
+    private fun dateFormat(year: Int, month: Int, day: Int) {
+        datePicked = "$year-"
+        datePicked += if (month < 9) "0${(month + 1)}-" else "${(month + 1)}-"
+        datePicked += if (day < 10) "0$day" else "$day"
+    }
+
     private fun beginSearch() {
-        val intent = ScheduleActivity.newIntent(this, "ZRH","FRA","2019-01-23")
+        val intent = ScheduleActivity.newIntent(
+            this,
+            edit_text_search_origin.text.toString(),
+            edit_text_search_destination.text.toString(),
+            datePicked
+        )
         startActivity(intent)
     }
 
